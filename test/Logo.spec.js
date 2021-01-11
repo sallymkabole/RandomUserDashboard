b244beeab034cf884e91a4892b04d61148a71a8b
sallythetechie <sllmusanga7@gmail.com>
2021-01-11 13:53:45 +0300

    Add test to check component rendering

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,18 @@
-import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import { mount, shallowMount } from "@vue/test-utils";
+import Logo from "@/components/Logo.vue";
 
-describe('Logo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
+const factory = () => {
+  return shallowMount(Logo, {});
+};
+
+describe("Logo", () => {
+  test("is a Vue instance", () => {
+    const wrapper = mount(Logo);
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test("renders properly", () => {
+    const wrapper = factory();
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
